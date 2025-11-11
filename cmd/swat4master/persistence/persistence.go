16fd1e143687da4e0fa2757f0bd67447dd13aa4e
Sergei Khoroshilov <kh.sergei@gmail.com>
2025-11-11 01:12:15 +0100

    fix(cmd/persistence): disable maintenance notifications for redis client

--- cmd/swat4master/persistence/persistence.go
+++ cmd/swat4master/persistence/persistence.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/redis/go-redis/v9/maintnotifications"
 	"go.uber.org/fx"
 )
 
@@ -21,6 +22,12 @@ func Provide(cfg Config, lc fx.Lifecycle) (Persistence, error) {
 	opts, err := redis.ParseURL(cfg.RedisURL)
 	if err != nil {
 		return Persistence{}, err
+	}
+
+	// Disable maintenance notifications
+	// https://github.com/redis/go-redis/issues/3536
+	opts.MaintNotificationsConfig = &maintnotifications.Config{
+		Mode: maintnotifications.ModeDisabled,
 	}
 
 	redisClient := redis.NewClient(opts)
